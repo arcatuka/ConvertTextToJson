6312357740023973bdf99977f8caf050c91b7dcd
TK <khangtt1803@gmail.com>
2022-05-30 22:04:42 +0700

    Update code

--- ChangeTextToJson/Program.cs
+++ ChangeTextToJson/Program.cs
@@ -8,12 +8,16 @@ namespace ChangeTextToJson
 {
     class Program
     {
-        
+
         static void Main(string[] args)
         {
-            string filePath = @"C:\\Users\\arcan\\OneDrive\\Desktop\\Vscode BaiTap\\ChangeTextToJson\\ChangeTextToJson\\Test.txt";
-            string _path = @"C:\\Users\\arcan\\OneDrive\\Desktop\\Vscode BaiTap\\ChangeTextToJson\\ChangeTextToJson\\Employee.json";
-            List<Employee> employees = new List<Employee>();  
+            
+            string filePath = "Test.txt";
+            string JsonPath = "Employee.json";
+            List<Employee> Employees = new List<Employee>();
+            
+            /*
+            List<Employee> Employees = new List<Employee>();  
             List<string> lines = File.ReadAllLines(filePath).ToList();
             foreach (string line in lines)
             {
@@ -22,14 +26,56 @@ namespace ChangeTextToJson
                 employee.Name = parts[0];
                 employee.Age = int.Parse(parts[1]);
                 employee.Job = parts[2];
-                employees.Add(employee);
-                
+                Employees.Add(employee);
             }
-            
-            var jsonWrite = JsonConvert.SerializeObject(employees, Formatting.Indented);
-            File.WriteAllText(_path, jsonWrite);
-                
-                
-        }
+            */
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    List<string> lines = File.ReadAllLines(filePath).ToList();
+                    foreach (string line in lines)
+                    {
+                        string[] parts = line.Split(',');
+                        Employee employee = new Employee();
+                        employee.Name = parts[0];
+                        employee.Age = int.Parse(parts[1]);
+                        employee.Job = parts[2];
+                        Employees.Add(employee);
+                    }
+                }
+                else if (!File.Exists(filePath))
+                {
+                    using (var WriteText = new StreamWriter(filePath, true))
+                    {
+                        WriteText.Write("New Text File created");
+                        WriteText.Close();
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+
+            var jsonWrite = JsonConvert.SerializeObject(Employees, Formatting.Indented);
+            try
+            {
+                if(File.Exists(JsonPath))
+                {
+                    File.WriteAllText(JsonPath, jsonWrite);
+                }    
+                else if(!File.Exists(JsonPath))
+                {
+                    using (var WriteJson = new StreamWriter(JsonPath, true))
+                    {
+                        WriteJson.Write(jsonWrite.ToString());
+                        WriteJson.Close();
+                    }
+
+                }
+            }
+            catch { }
+        }  
     }
 }
